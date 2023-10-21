35bd851548b8b15a7b36f29de1f1cbefdbf8c210
CHAD-DEV\Medium Chad <cbrathwaite93@gmail.com>
2023-10-21 08:54:11 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hi! Let's find the area of the rectangle!");
+class recarea
+{ static void Main()
+    { Console.WriteLine("What is the length");
+        string lengthInput = Console.ReadLine();
+        int length = int.Parse(lengthInput);
+
+        Console.WriteLine("What is the width");
+        string widthInput = Console.ReadLine();
+        int width = int.Parse(widthInput);
+        int area = length * width;
+        Console.WriteLine("The area of the rectangle is:" + area);
+
+    }
+}
